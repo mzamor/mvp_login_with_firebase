e0b185f11767712c2b7b84c8e137610db4c8d891
marianozamora <zamoramariano@hotmail.com>
2020-01-15 17:22:23 -0300

    it was added the Firebase notification messenger service

--- app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.loginconmvpsociedadandroide.presentation.main.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.loginconmvpsociedadandroide.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
