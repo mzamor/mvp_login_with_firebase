74cffb290b8ed7da3d70e4651d550122760a8b2c
marianozamora <zamoramariano@hotmail.com>
2020-03-12 19:38:48 -0300

    boton de notificaciones

--- app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.example.loginconmvpsociedadandroide.presentation.main.view
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.loginconmvpsociedadandroide.R
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
+        bt_notifications.setOnClickListener{
+
+        }
     }
+
+
 }
