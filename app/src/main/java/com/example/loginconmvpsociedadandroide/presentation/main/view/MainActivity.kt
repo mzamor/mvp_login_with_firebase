a9ad7794e1a526987f23509ab5a018b73f4a59c9
marianozamora <zamoramariano@hotmail.com>
2020-03-13 17:44:26 -0300

    se agrego remote config

--- app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/example/loginconmvpsociedadandroide/presentation/main/view/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.loginconmvpsociedadandroide.presentation.main.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginconmvpsociedadandroide.R
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
 
+    var remoteConfig = FirebaseRemoteConfig.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(10)
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+
 
         bt_notifications.setOnClickListener{
-
+            remoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("notificationSuccess", "Config params updated: ${remoteConfig.getString("push_notifications")}")
+                    } else {
+                        Log.d("notificationFail", "fail")
+                    }
+                }
         }
     }
-
-
 }
